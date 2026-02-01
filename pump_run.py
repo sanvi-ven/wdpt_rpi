afb034541e66fee3b5a00276768cf3bc7f6f3b44
Sanvi Venkayala <svenkayala1@wpi.edu>
2026-02-01 14:46:49 -0500

    pump-run

---
+++ pump_run.py
@@ -0,0 +1,16 @@
+import time
+import serial
+
+# This is usually ttyACM0 or ttyUSB0
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+time.sleep(2)  # let Arduino reset
+
+print("Pump ON")
+ser.write(b'1')
+time.sleep(12)
+
+print("Pump OFF")
+ser.write(b'0')
+
+ser.close()
+#----
