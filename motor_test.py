65c11fae385caaadad3b85d49802476e206966b6
Sanvi Venkayala <svenkayala1@wpi.edu>
2026-01-31 22:03:33 -0500

    with-gpiozero

--- motor_test.py
+++ motor_test.py
@@ -1,34 +1,29 @@
-import RPi.GPIO as GPIO
+from gpiozero import PWMOutputDevice, DigitalOutputDevice
 import time
 
-# Pin definitions
-AIN1 = 17
-AIN2 = 27
-PWMA = 18
-STBY = 22
+# Direction pins
+AIN1 = DigitalOutputDevice(17)
+AIN2 = DigitalOutputDevice(27)
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup([AIN1, AIN2, STBY], GPIO.OUT)
-GPIO.setup(PWMA, GPIO.OUT)
+# PWM speed pin
+PWMA = PWMOutputDevice(18)
 
-# Enable driver
-GPIO.output(STBY, GPIO.HIGH)
+# Standby pin
+STBY = DigitalOutputDevice(22)
 
-# PWM setup
-pwm = GPIO.PWM(PWMA, 1000)  # 1 kHz
-pwm.start(0)
+# Enable motor driver
+STBY.on()
 
-print("Motor ON")
-GPIO.output(AIN1, GPIO.HIGH)
-GPIO.output(AIN2, GPIO.LOW)
-pwm.ChangeDutyCycle(50)  # 50% speed
+print("Motor forward")
+AIN1.on()
+AIN2.off()
+PWMA.value = 0.5   # 50% speed
 
 time.sleep(2)
 
-print("Motor OFF")
-pwm.ChangeDutyCycle(0)
-GPIO.output(AIN1, GPIO.LOW)
-GPIO.output(AIN2, GPIO.LOW)
+print("Motor stop")
+PWMA.value = 0
+AIN1.off()
+AIN2.off()
 
-pwm.stop()
-GPIO.cleanup()
+STBY.off()
