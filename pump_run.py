091d68ee7314822abf985d5da83f2f6de3cabe86
Sanvi Venkayala <svenkayala1@wpi.edu>
2026-02-02 23:10:58 -0500

    y

--- pump_run.py
+++ pump_run.py
@@ -24,7 +24,7 @@ except Exception as e:
 
 print(f"{_now()}  Sleeping 12 seconds")
 start = time.time()
-time.sleep(12)
+time.sleep(5)
 elapsed = time.time() - start
 print(f"{_now()}  Woke after {elapsed:.2f} seconds")
 
